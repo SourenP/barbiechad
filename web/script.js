05c5b233c6c696a26e0e3d35a62b94e6330a0445
Souren <souren@papazian.info>
2015-12-03 02:05:57 -0500

    rearranged files and added skeleton + api call

---
+++ web/script.js
@@ -0,0 +1,77 @@
+var API_KEY = "O3YEHGYZNFQA77X5E"
+var CONSUMER_KEY = "1e3406ea62e381dfd5201f1ec84592a9"
+var SHARED_SECRET = "69HZAfaGTdyyizIoxDW0rA"
+
+$(document).ready(function () {
+  console.log("yolo")
+  getTracks('country', 'tempo', 100)
+})
+
+function getTracks(style, metric, value) {
+  var url = "http://developer.echonest.com/api/v4/song/search"
+  var data = {
+    'api_key': API_KEY,
+    'format': 'json',
+    'results': 1,
+    'style': style
+  }
+
+  // Check if metric is valid
+  min_max = getRange(metric, value)
+  if (min_max.length == 0) {
+    console.error("Invalid metric")
+    return
+  }
+
+  // Add metric to call
+  data['min_' + metric] = min_max[0]
+  data['max_' + metric] = min_max[1]
+
+  $.ajax({
+    url: url,
+    data: data,
+    success: function (response) {
+      console.log(response)
+    }
+  });
+}
+
+function getRange(metric, value) {
+  switch (metric) {
+    case "tempo":
+      margin = 500*10/100
+      min = value - margin
+      if (min <= 0) min = 0.01;
+      max = value + margin
+      if (max >= 500) max = 499.99;
+      return [min, max]
+      break;
+    case "loudness":
+      margin = 200*10/100
+      min = value - margin
+      if (min <= -100) min = -99.99;
+      max = value + margin
+      if (max >= 100) max = 99.99;
+      return [min, max]
+      break;
+    case "energy":
+      margin = 1*10/100
+      min = value - margin
+      if (min <= 0) min = 0.01;
+      max = value + margin
+      if (max >= 1) max = 0.99;
+      return [min, max]
+      break;
+    case "danceability":
+      margin = 1*10/100
+      min = value - margin
+      if (min <= 0) min = 0.01;
+      max = value + margin
+      if (max >= 1) max = 0.99;
+      return [min, max]
+      break;
+    default:
+      // Error
+      return [];
+  }
+}
